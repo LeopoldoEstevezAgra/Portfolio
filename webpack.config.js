9c31b13eb8a4844cb4fee12e23328279297adebf
Leopoldo Estevez <leopoldoestevezagra@gmail.com>
2019-07-02 21:38:15 +0200

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+var Encore = require('@symfony/webpack-encore');
+
+var webpack = require('webpack');
+
+
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('web/build/')
+
+    //Versioning changes the different file names to a hash name , making all cache irrelevant
+
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
+    /*
+     * ENTRY CONFIG
+     *
+     * Add 1 entry for each "page" of your app
+     * (including one that's included on every page - e.g. "app")
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
+     */
+
+    //SCCS
+    //.addStyleEntry('css/app', './assets/css/app.scss')
+
+    //JS
+
+    .addEntry('js/app', './assets/js/app.js')
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+
+
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning()
+
+    .autoProvidejQuery()
+
+
+    // uncomment if you use Sass/SCSS files
+    .enableSassLoader(function (sassOptions) { }, {
+        resolveUrlLoader: false
+    })
+
+    .enableSassLoader()
+
+    // uncomment if you're having problems with a jQuery plugin
+
+
+
+    ;
+
+module.exports = Encore.getWebpackConfig();
