cd89402bd93db8df5f9b7f1d798e41d55c21b86d
Leopoldo Estevez <leopoldoestevezagra@gmail.com>
2020-08-04 08:20:21 +0200

    Added : dotenv secret key management

--- api/server/config/mongooseConnect.js
+++ api/server/config/mongooseConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://user:pass@cluster0.zcqwd.mongodb.net/vue-portfolio?retryWrites=true&w=majority",
+mongoose.connect(process.env.MONGO_URI,
   {
     useNewUrlParser: true ,
     useUnifiedTopology: true,
