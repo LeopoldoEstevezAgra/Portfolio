b1ee3015bbc794504de806fcf9f98ba843e33d0e
Leopoldo Estevez <leopoldoestevezagra@gmail.com>
2020-08-05 09:01:42 +0200

    Added : Proxy on vueconfig

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,12 @@
+const path = require('path');
+
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  devServer: {
+    proxy: {
+      '': {
+        target: 'http://localhost:5000'
+      }
+    }
+  }
 };
