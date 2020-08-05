e38d418aec61991d93765926017e62c76cd39d56
Leopoldo Estevez <leopoldoestevezagra@gmail.com>
2020-08-05 10:07:16 +0200

    Heroku error

--- api/server/index.js
+++ api/server/index.js
@@ -19,7 +19,7 @@ app.use('/projects', projects)
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname + '/public/'))
 
-  app.get(/.*/,(req, res) => res.sendFile(__dirname + '../public/index.html'));
+  app.get(/.*/,(req, res) => res.sendFile('/public/index.html'));
 }
 
 const port = process.env.PORT || 5000;
